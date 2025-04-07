2861b64339b85f3f9d319139ebd2597f9b02ef28
MonkeyIQDown241 <646255079@qq.com>
2025-04-07 16:57:40 +0800

    real_lidar

---
+++ SUPER/mars_uav_sim/perfect_drone_sim/src/real_drone_node.cpp
@@ -0,0 +1,14 @@
+#include "real_lidar_drone.hpp"
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "real_drone_node");
+    ros::NodeHandle nh("~");
+    
+    RealLidarDrone drone(nh);
+    
+    ros::AsyncSpinner spinner(4);
+    spinner.start();
+    
+    ros::waitForShutdown();
+    return 0;
+}
